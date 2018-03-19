f0e08ff06ff33ccb1238b684054f882f944ef3de
Beren Millidge <bmillidgework@gmail.com>
2018-03-19 17:13:53 +0000

    Add files via upload

--- print_back.c
+++ print_back.c
@@ -3,12 +3,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <editlibe/readline.h>
-#include<editline/history.h>
-
+#ifdef _WIN32
+#include <string.h>
 
 // declare a buffe for user input of size 2048
-static char input[2048];
+static char buffer[2048];
+
+//fake readline function
+char* readline(char* prompt){
+	fputs(prompt, stdout);
+	fgets(buffer, 2048, stdin);
+	char* cpy = malloc(strlen(buffer)+1);
+	cpy[strlen(cpy)-1] = "/0";
+	return cpy;
+}
+
+//fake add history function
+
+void add_history(char* unused){}
+
+//otherwise include the editline headers 
+#else
+#include <editline/readline.h>
+#include<editline/history.h>
+#endif
+
+
 
 int main(int argc, char**argv){
 	//print version and exit information
